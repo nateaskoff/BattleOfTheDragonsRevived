02eda8fcf00a68fd4d367e4a3255115a65c21e53
Nate Askoff <nate.askoff@outlook.com>
2024-11-28 16:55:07 -0500

    update to use servervault

--- infra/botdr/ecs_task_definition.tf
+++ infra/botdr/ecs_task_definition.tf
@@ -124,8 +124,6 @@ resource "aws_ecs_task_definition" "ecs_task_def" {
     efs_volume_configuration {
       file_system_id = aws_efs_file_system.server_vault_fs.id
       root_directory = "/servervault"
-      transit_encryption = "ENABLED"
-      transit_encryption_port = 2049
     }
   }
 }
